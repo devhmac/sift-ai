3c5c839a058110b59c658716379b839e027b96ca
Devin MacGillivray <devhmac@gmail.com>
2023-10-14 23:06:46 -0600

    landing wrapup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -91,7 +91,7 @@ export default function Home() {
               Upload to start chatting
             </h2>
             <p className="mt-4 text-lg text-gray-600">
-              Instantly access meaningful data at scale.
+              Instantly access meaningful data at scale via a semantic layer.
             </p>
           </div>
         </div>
@@ -99,17 +99,56 @@ export default function Home() {
         {/* steps */}
         <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
           <li className="md:flex-1">
-            <div className="flex flex-col space-y-2 border-1-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:pd-0 md:pl-0 md:pt-4">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:pd-0 md:pl-0 md:pt-4">
               <span className="text-sm font-medium text-green-600">Step 1</span>
               <span className="text-xl font-semibold">
                 Sign up for an account
               </span>
               <span className="mt-2 text-zinc-700">
-                Start for free or subscribe to pro
+                Start for free or subscribe to{" "}
+                <Link
+                  href="/pricing"
+                  className="text-green-700 underline underline-offset-2"
+                >
+                  Pro
+                </Link>
+              </span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:pd-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-green-600">Step 2</span>
+              <span className="text-xl font-semibold">Upload your PDF </span>
+              <span className="mt-2 text-zinc-700">
+                AI will analyze your file
+              </span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:pd-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-green-600">Step 3</span>
+              <span className="text-xl font-semibold">Ask Questions</span>
+              <span className="mt-2 text-zinc-700">
+                Gain insights instantly
               </span>
             </div>
           </li>
         </ol>
+
+        <div className="mx-auto max-w-6xl px-6 lg:px-8">
+          <div className="mt-16 flow-root sm:mt-24">
+            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+              <Image
+                className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+                src="/file-upload-preview.jpg"
+                alt="Upload Preview"
+                width={1419}
+                height={732}
+                quality={100}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
