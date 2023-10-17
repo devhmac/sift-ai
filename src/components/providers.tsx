3ef9e49d2a7ff811c68d81c044215d3fb9e5c733
Devin MacGillivray <devhmac@gmail.com>
2023-10-16 20:37:06 -0600

    more trpc

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,10 +1,27 @@
 "use client";
-import { QueryClient } from "@tanstack/react-query";
-import { useState } from "react";
+// trpc setup, all api requests are going to be sent though this file and routed
+import { trpc } from "@/app/_trpc/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { Children, PropsWithChildren, useState, ReactNode } from "react";
 
-const Providers = () => {
+const Providers = ({ children }: { children: ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient());
-  const [trpcClient] = useState(() =>  );
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: "http://localhost:3000/api/trpc",
+        }),
+      ],
+    })
+  );
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  );
 };
 
-export default Providers
+export default Providers;
