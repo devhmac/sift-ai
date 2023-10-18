5ecf44cb62f3a6a7c99e1b16f2719805be1b8cee
Devin MacGillivray <devhmac@gmail.com>
2023-10-17 20:04:40 -0600

    finishes check auth api call, and creates user if failed

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -3,16 +3,27 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
+import { db } from "@/db";
 
 export const appRouter = router({
   //can do querys (generally get requests etc) and mutations (posts etc)
-  authCallback: publicProcedure.query(() => {
+  authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
     const user = getUser();
 
     if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
 
     //is user in DB, no create, yes return true
+    const dbUser = await db.user.findFirst({
+      where: { id: user.id },
+    });
+
+    if (!dbUser) {
+      await db.user.create({
+        data: { id: user.id, email: user.email },
+      });
+    }
+
     return { success: true };
   }),
 });
