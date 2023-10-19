4c7f2b997f575d9b3bafb8b6db22687d7347d573
Devin MacGillivray <devhmac@gmail.com>
2023-10-18 19:09:15 -0600

    adding nice upload button

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -1,4 +1,27 @@
+"use client";
+
+import { useState } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import { Button } from "./ui/button";
+
 const UploadButton = () => {
-  return <div>button</div>;
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(n) => {
+        if (!n) {
+          setIsOpen(n);
+        }
+      }}
+    >
+      {/* asChild to not wrap our button component in another button */}
+      <DialogTrigger onClick={() => setIsOpen(true)} asChild>
+        <Button> Upload PDF</Button>
+      </DialogTrigger>
+      <DialogContent>Test example</DialogContent>
+    </Dialog>
+  );
 };
 export default UploadButton;
