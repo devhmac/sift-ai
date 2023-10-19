25224b68b2b8081e30fdb330c971c78d5726f264
Devin MacGillivray <devhmac@gmail.com>
2023-10-18 19:16:15 -0600

    placerholder for files

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -8,6 +8,7 @@ const Dashboard = () => {
         <h1 className="mb-3 font-bold text-5xl text-gray-900">My Files</h1>
         <UploadButton />
       </div>
+      {/* display files */}
     </main>
   );
 };
