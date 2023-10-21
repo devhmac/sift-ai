61581f3a01207f9a30e64e007859810e5fea0678
Devin MacGillivray <devhmac@gmail.com>
2023-10-20 22:42:44 -0600

    adds empty state to dashboard

--- README.md
+++ README.md
@@ -41,7 +41,7 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/deploym
 - NextJS
 - Tailwindcss
 - Shadcn
-  -tRPC
+  -tRPC -- wrapper over tanstack
 
   prisma - database wrapper
 
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,9 +1,11 @@
+"use client";
+import { Ghost } from "lucide-react";
 import { Button } from "./ui/button";
 import UploadButton from "./UploadButton";
 import { trpc } from "@/app/_trpc/client";
 
 const Dashboard = () => {
-  const { data } = trpc.getUserFiles.useQuery();
+  const { data: files, isLoading } = trpc.getUserFiles.useQuery(); //this is client side util
 
   return (
     <main className="mx-auto mw-7xl md:p-10">
@@ -12,6 +14,17 @@ const Dashboard = () => {
         <UploadButton />
       </div>
       {/* display files */}
+      {files && files?.length !== 0 ? (
+        <div></div>
+      ) : isLoading ? (
+        <div></div>
+      ) : (
+        <div className="mt-16 flex flex-col items-center gap-2">
+          <Ghost className="h-8 w-8 text-zinc-800" />
+          <h3 className="font-semibold text-xl">Just An Empty Void</h3>
+          <p>Upload your first PDF</p>
+        </div>
+      )}
     </main>
   );
 };
