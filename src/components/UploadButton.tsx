1678f1a32454a50ba765a8f52d048b2ca1e257e2
Devin MacGillivray <devhmac@gmail.com>
2023-10-23 20:08:41 -0600

    buildin drag and drop upload

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -7,12 +7,28 @@ import Dropzone from "react-dropzone";
 
 const UploadDropzone = () => {
   return (
-    <Dropzone multiple={false}>
+    <Dropzone
+      multiple={false}
+      onDrop={(acceptedFile) => {
+        console.log(acceptedFile);
+      }}
+    >
       {({ getRootProps, getInputProps, acceptedFiles }) => (
         <div
           {...getRootProps()}
           className="border h-64 m-4 border-dashed border-gray-300 rounded-lg"
-        ></div>
+        >
+          <div className="flex items-center justify-center h-full w-full">
+            <label
+              htmlFor="dropzone-file"
+              className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointe bg-gray-50 hover:bg-gray-100"
+            >
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                Example
+              </div>
+            </label>
+          </div>
+        </div>
       )}
     </Dropzone>
   );
