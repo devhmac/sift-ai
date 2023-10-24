40dd07372b4a7a7fa549597f1ce4ece910433212
Devin MacGillivray <devhmac@gmail.com>
2023-10-23 20:00:51 -0600

    building filedrop

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -3,6 +3,20 @@
 import { useState } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import { Button } from "./ui/button";
+import Dropzone from "react-dropzone";
+
+const UploadDropzone = () => {
+  return (
+    <Dropzone multiple={false}>
+      {({ getRootProps, getInputProps, acceptedFiles }) => (
+        <div
+          {...getRootProps()}
+          className="border h-64 m-4 border-dashed border-gray-300 rounded-lg"
+        ></div>
+      )}
+    </Dropzone>
+  );
+};
 
 const UploadButton = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -20,7 +34,9 @@ const UploadButton = () => {
       <DialogTrigger onClick={() => setIsOpen(true)} asChild>
         <Button> Upload PDF</Button>
       </DialogTrigger>
-      <DialogContent>Test example</DialogContent>
+      <DialogContent>
+        <UploadDropzone />
+      </DialogContent>
     </Dialog>
   );
 };
