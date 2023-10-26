3d068a0280f3404c74d9db47939c5a6ce9a6e4c6
Devin MacGillivray <devhmac@gmail.com>
2023-10-26 12:45:09 -0600

    More upload think boilerplate, getting custom hook type

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,3 +1,4 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 
 const f = createUploadthing();
@@ -10,21 +11,18 @@ export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "4MB" } })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
+      const { getUser } = getKindeServerSession();
+      const user = getUser();
+      if (!user || !user.id) throw new Error("Unauthorized");
+
       // This code runs on your server before upload
-      const user = await auth(req);
 
       // If you throw, the user will not be able to upload
-      if (!user) throw new Error("Unauthorized");
 
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
       return { userId: user.id };
     })
-    .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
-      console.log("Upload complete for userId:", metadata.userId);
-
-      console.log("file url", file.url);
-    }),
+    .onUploadComplete(async ({ metadata, file }) => {}),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
