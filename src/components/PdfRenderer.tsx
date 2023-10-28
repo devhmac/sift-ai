2143eb62cb09cc2fd33b474687813966c4c698aa
Devin MacGillivray <devhmac@gmail.com>
2023-10-27 23:24:07 -0600

    adding pdf loading spinner

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,12 +1,20 @@
-import { Document, Page } from "react-pdf";
+"use client";
+import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/TextLayer.css";
 import "react-pdf/dist/Page/AnnotationLayer.css";
+import { Loader2 } from "lucide-react";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+import { useToast } from "./ui/use-toast";
 
 interface PdfRendererProps {
   url: string;
 }
 
 const PdfRenderer = ({ url }: PdfRendererProps) => {
+  const { toast } = useToast();
+
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center ">
       <div className="h-14 w-full border-b border-zinc-200 items-center justify-between px-2">
@@ -15,7 +23,22 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
 
       <div className="flex-1 w-full max-h-screen">
         <div>
-          <Document file={url} className="max-h-full">
+          <Document
+            loading={
+              <div className="flex justify-center">
+                <Loader2 className="my-24 h-6 w-6 animate-spin" />
+              </div>
+            }
+            onLoadError={() => {
+              toast({
+                title: "Error loading PDF",
+                description: "Please try again",
+                variant: "destructive",
+              });
+            }}
+            file={url}
+            className="max-h-full"
+          >
             <Page pageNumber={1} />
           </Document>
         </div>
