a7881d836b94920e86b09e392dfee478a8e289fd
Devin MacGillivray <devhmac@gmail.com>
2023-11-01 21:18:49 -0600

    adding route type validator and getting file in message route

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,3 +1,5 @@
+import { db } from "@/db";
+import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextRequest } from "next/server";
 
@@ -12,4 +14,17 @@ export const POST = async (req: NextRequest) => {
   const { id: userId } = user;
 
   if (!userId) return new Response("Unauthorized", { status: 401 });
+
+  const { fileId, message } = SendMessageValidator.parse(body);
+
+  const file = await db.file.findFirst({
+    where: {
+      id: fileId,
+      userId: userId,
+    },
+  });
+
+  if (!file) return new Response("Not Found", { status: 404 });
+
+  // await db.message
 };
--- src/lib/validators/SendMessageValidator.ts
+++ src/lib/validators/SendMessageValidator.ts
@@ -1,2 +1,6 @@
 import { z } from "zod";
 //this is going to enforce a constant schema for all messages being sent through api route
+export const SendMessageValidator = z.object({
+  fileId: z.string(),
+  message: z.string(),
+});
