5a49104b6bfe398390ca24817bffe001a41b1812
Devin MacGillivray <devhmac@gmail.com>
2023-11-01 18:47:42 -0600

    more context algo

--- src/components/messaging/ChatContext.tsx
+++ src/components/messaging/ChatContext.tsx
@@ -1,4 +1,6 @@
-import { ReactNode, createContext } from "react";
+import { ReactNode, createContext, useState } from "react";
+import { useToast } from "../ui/use-toast";
+import { useMutation } from "@tanstack/react-query";
 
 type StreamResponse = {
   addMessage: () => void;
@@ -19,4 +21,36 @@ interface props {
   children: ReactNode;
 }
 
-export const ChatContextProvider = ({ fileId, children }: props) => {};
+export const ChatContextProvider = ({ fileId, children }: props) => {
+  const [message, setMessage] = useState<string>("");
+
+  const { toast } = useToast();
+
+  //not using TRPC because we want to stream back the JSON response, TRPC doesnt use json
+  const { mutate: sendMessage } = useMutation({
+    mutationFn: async ({ message }: { message: string }) => {
+      const response = await fetch("/api/message", {
+        method: "POST",
+        body: JSON.stringify({
+          fileId,
+          message,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to send message");
+      }
+
+      return response.body;
+    },
+  });
+
+  const addMessage = () => sendMessage({ message });
+
+  return (
+    <ChatContext.Provider
+      value={{ addMessage, message, handleInputChange, isLoaded }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
