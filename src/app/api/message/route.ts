11e15dc65ad173d5a2daf09a4785c2922b48695d
Devin MacGillivray <devhmac@gmail.com>
2023-11-04 17:47:02 -0600

    setting up response stream

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -5,6 +5,9 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { NextRequest } from "next/server";
 import { PineconeStore } from "langchain/vectorstores/pinecone";
+// import OpenAI from "openai";
+import { openai } from "@/lib/openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 
 export const POST = async (req: NextRequest) => {
   //message ask question endpoint
@@ -58,7 +61,7 @@ export const POST = async (req: NextRequest) => {
     orderBy: {
       createdAt: "asc",
     },
-    take: 8,
+    take: 6,
   });
 
   const formattedMessages = prevMessages.map((msg) => ({
@@ -66,5 +69,49 @@ export const POST = async (req: NextRequest) => {
     content: msg.text,
   }));
 
-  const response = await openai;
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    temperature: 0,
+    stream: true,
+    messages: [
+      {
+        role: "system",
+        content:
+          "Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.",
+      },
+      {
+        role: "user",
+        content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
+        
+  \n----------------\n
+  
+  PREVIOUS CONVERSATION:
+  ${formattedMessages.map((message) => {
+    if (message.role === "user") return `User: ${message.content}\n`;
+    return `Assistant: ${message.content}\n`;
+  })}
+  
+  \n----------------\n
+  
+  CONTEXT:
+  ${results.map((r) => r.pageContent).join("\n\n")}
+  
+  USER INPUT: ${message}`,
+      },
+    ],
+  });
+
+  const stream = OpenAIStream(response, {
+    async onCompletion(completion) {
+      await db.message.create({
+        data: {
+          text: completion,
+          isUserMessage: false,
+          fileId,
+          userId,
+        },
+      });
+    },
+  });
+  return new StreamingTextResponse(stream);
 };
