3001348d73575c7ab28efc77fd93092046ae71e8
Devin MacGillivray <devhmac@gmail.com>
2023-11-05 11:18:01 -0700

    building out message rendering

--- src/components/messaging/Messages.tsx
+++ src/components/messaging/Messages.tsx
@@ -1,7 +1,62 @@
-const Messages = () => {
-  // const {} = trpc;
+import { trpc } from "@/app/_trpc/client";
+import { INFINITE_QUERY_LIMIT } from "@/app/config/infinite-query";
+import { Loader2 } from "lucide-react";
 
-  return <div className=" flex justify-center items-end"></div>;
+interface MessagesProps {
+  fileId: string;
+}
+
+const Messages = ({ fileId }: MessagesProps) => {
+  const { data, isLoading, fetchNextPage } =
+    trpc.getFileMessages.useInfiniteQuery(
+      {
+        fileId,
+        limit: INFINITE_QUERY_LIMIT,
+      },
+      {
+        getNextPageParam: (lastPage) => lastPage?.nextCursor,
+        keepPreviousData: true,
+      }
+    );
+
+  const messages = data?.pages.flatMap((page) => page.messages);
+  const loadingMessage = {
+    createdAt: new Date().toISOString(),
+    id: "loading_message",
+    isUserMessage: false,
+    text: (
+      <span className="flex h-full items-center justify-center">
+        <Loader2 className="h-4 w-4 animate-spin" />
+      </span>
+    ),
+  };
+
+  const combinedMessages = [
+    ...(true ? [loadingMessage] : []),
+    ...(messages ?? []),
+  ];
+
+  return (
+    <div className=" flex max-h-[calc(100vh)-3.5rem-7rem] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-rounded scrollbar-thumb-green scrollbar-track-green-lighter scrollbar-w-2 scrolling-touch">
+      {combinedMessages && combinedMessages.length > 0 ? (
+        combinedMessages.map((message, i) => {
+          const isNextMessageSamePerson =
+            combinedMessages[i - 1]?.isUserMessage ===
+            combinedMessages[i]?.isUserMessage;
+
+          if (i === combinedMessages.length - 1) {
+            return <Message />;
+          } else {
+            <Message />;
+          }
+        })
+      ) : isLoading ? (
+        <div></div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
 };
 
 export default Messages;
