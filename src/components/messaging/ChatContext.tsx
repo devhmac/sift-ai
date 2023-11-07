2e50ef34c27229c4adb2e33836a663dc1ea9dbc2
Devin MacGillivray <devhmac@gmail.com>
2023-11-06 19:57:43 -0700

    building optimistic updating

--- src/components/messaging/ChatContext.tsx
+++ src/components/messaging/ChatContext.tsx
@@ -1,6 +1,8 @@
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useRef, useState } from "react";
 import { useToast } from "../ui/use-toast";
 import { useMutation } from "@tanstack/react-query";
+import { trpc } from "@/app/_trpc/client";
+import { INFINITE_QUERY_LIMIT } from "@/app/config/infinite-query";
 
 type StreamResponse = {
   addMessage: () => void;
@@ -25,11 +27,14 @@ export const ChatContextProvider = ({ fileId, children }: props) => {
   const [message, setMessage] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const { toast } = useToast();
+  //gives access to helperfunctions on your routes
+  const utils = trpc.useContext();
 
+  const { toast } = useToast();
+  const backupMessage = useRef("");
   //not using TRPC because we want to stream back the JSON response, TRPC doesnt use json
 
-  //paking post request
+  //making post request
   const { mutate: sendMessage } = useMutation({
     mutationFn: async ({ message }: { message: string }) => {
       const response = await fetch("/api/message", {
@@ -44,6 +49,47 @@ export const ChatContextProvider = ({ fileId, children }: props) => {
       }
 
       return response.body;
+    },
+    onMutate: async () => {
+      //saving current message so we have it in case upload failure
+      backupMessage.current = message;
+      setMessage("");
+
+      // first - pause getting messages
+      await utils.getFileMessages.cancel();
+
+      //then get previous messages
+      const previousMessages = utils.getFileMessages.getInfiniteData();
+
+      //
+      utils.getFileMessages.setInfiniteData(
+        {
+          fileId,
+          limit: INFINITE_QUERY_LIMIT,
+        },
+        (old) => {
+          if (!old) {
+            return {
+              //reactquery handles the infinite data hook and needs these objects
+              pages: [],
+              pageParams: [],
+            };
+          }
+          // getting the pages of messages
+          let newPages = [...old.pages];
+          // grab first item in array which contains the latest 10 messages in chat
+          let latestPage = newPages[0]!;
+
+          latestPage.messages = [
+            {
+              createdAt: new Date().toISOString(),
+              id: crypto.randomUUID(),
+              text: message,
+              isUserMessage: true,
+            },
+          ];
+        }
+      );
     },
   });
 
