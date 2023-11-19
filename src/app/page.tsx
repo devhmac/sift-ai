a60d39591bdf4bdbb61eafd923a423644136632f
Devin MacGillivray <devhmac@gmail.com>
2023-11-19 14:10:37 -0700

    small metadata updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
                 <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
                   <Image
                     className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
-                    src="/dashboard-preview.jpg"
+                    src="/dashboard-preview.png"
                     alt="Product Preview"
                     width={1364}
                     height={866}
